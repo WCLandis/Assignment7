5b54389f8eac06ff183b59b656caf0f4486c1926
WCLandis <95384790+WCLandis@users.noreply.github.com>
2021-12-02 09:07:04 -0500

    Update peerReviewErrors.py

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -9,26 +9,29 @@ import random
 import time
 
 def displayIntro():
-#This message originally displayed tabbed over, and visually unappealing and looked shifted. I removed the unneeded quotations and spaces to correct this mistake.
+#Line 13 originally displayed tabbed over, and visually unappealing and looked shifted. 
 	print('You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you. The other dragon is greedy and hungry, and will eat you on sight.')
+#I removed the unneeded quotations and spaces to correct this mistake. This correction reformats the text so that it is clean and neat.
 	print()
-#When attempting to run this program for the first time, I found this error: inconsistent use of tabs and spaces in indentation. It appears that instead of using the tab, spacebar was used, which caused inconsistent formatting. I fixed the error by backspacing and adding the tab.
 def chooseCave():
+#When attempting to run this program for the first time, I found this error: inconsistent use of tabs and spaces in indentation. 
 	cave = ''
+#It appears that instead of using the tab, spacebar was used, which caused inconsistent formatting. I fixed the error by backspacing and adding the tab.
 	while cave != '1' and cave != '2':
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
-#Line 24 returned the following error: NameError: name 'caves' is not defined. I fixed this by changing the name to 'cave' to return the correct variable.
+#Line 24 returned the following error: NameError: name 'caves' is not defined. 
 	return cave
-
+#I fixed this by changing the name to 'cave' because 'caves' was returning the incorrect variable.
 def checkCave(chosenCave):
 	print('You approach the cave...')
 	#sleep for 2 seconds
 	time.sleep(2)
 	print('It is dark and spooky...')
 	#sleep for 2 seconds
-#In line 31, the comment states that sleep should last for a total of two seconds, it was originally set to three seconds. I corrected this error by changing it to two.
+#In line 31, the comment states that sleep should last for a total of two seconds, it was originally set to three seconds. 
 	time.sleep(2)
+#I corrected this error by changing the time interval to 2 seconds to match the intended sleep time.
 	print('A large dragon jumps out in front of you! He opens his jaws and...')
 	print()
 	#sleep for 2 seconds
@@ -37,21 +40,26 @@ def checkCave(chosenCave):
 
 	if chosenCave == str(friendlyCave):
 		print('Gives you his treasure!')
-#Line 42 revealed the following error: SyntaxError: Missing parentheses in call to 'print.' I added parenthesis around 'Gobbles you down in one bite' to resolve this error.
+#Line 42 revealed the following error: SyntaxError: Missing parentheses in call to 'print.' 
 	else:
 		print('Gobbles you down in one bite!')
-
+#I added parenthesis around 'Gobbles you down in one bite' to resolve this error, because the print function requires partenthesis in its parameters.
 playAgain = 'yes'
-#Line 45 had the following error: SyntaxError: invalid syntax. I fixed this error by adding an additional '=' before 'yes' and before 'y'
+#Line 45 had the following error: SyntaxError: invalid syntax. 
 while playAgain == 'yes' or playAgain == 'y':
+#I fixed this error by adding an additional '=' before 'yes' and before 'y.' This was necessary because the equality operator requires two equal signs.
 	displayIntro()
-#Line 49 had the following error: NameError: name 'choosecave' is not defined. The choosecave function was not written in camel case as it is defined. I rewrote it as 'chooseCave' to correct this error.
+#Line 49 had the following error: NameError: name 'choosecave' is not defined. 
 	caveNumber = chooseCave()
+#The choosecave function was not written in camel case as it is defined. I rewrote it as 'chooseCave' to correct this error.
 	checkCave(caveNumber)
 	print('Do you want to play again? (yes or no)') 
 	playAgain = input()
-#In line 55, the user is not given the 'n' option to receive the 'Thanks for playing' message. I added that for consistency.
+#In line 55, the user is not given the 'n' option to receive the 'Thanks for playing' message. 
 	if playAgain == "no" or playAgain == 'n':
-#In line 57, 'playing' was misspelled 'planing.' I corrected this misspell.
+#I added "or playAgain == 'n'" to maintain coding consistency since 'y' will restart the game.
+#line 57 orginally said 'Thanks for planing.' 
 		print("Thanks for playing")
+#Playing was misspelled, so I corrected this error.
+
 
